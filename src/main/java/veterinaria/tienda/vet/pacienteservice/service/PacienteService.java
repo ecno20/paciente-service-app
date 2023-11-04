3584c3406fe989625ba854d278143ea61e25f49a
Jonathan Díaz <144557398+ecno20@users.noreply.github.com>
2023-11-03 19:15:08 -0600

    Delete src directory

--- src/main/java/veterinaria/tienda/vet/pacienteservice/service/PacienteService.java
+++
@@ -1,7 +0,0 @@
-package veterinaria.tienda.vet.pacienteservice.service;
-
-import veterinaria.tienda.vet.pacienteservice.domain.Paciente;
-public interface PacienteService {
-	Paciente actualizarPaciente(String id, Paciente paciente);
-
-}
